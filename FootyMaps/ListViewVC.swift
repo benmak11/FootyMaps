a1f0d7d25f8f0543f404e18addae13b1f6240a88
Ben Makusha <btmakusha@gmail.com>
2017-04-20 09:56:52 -0400

    Started tableView context - FYI Missing for the other screens

--- FootyMaps/ListViewVC.swift
+++ FootyMaps/ListViewVC.swift
@@ -9,20 +9,29 @@
 import UIKit
 import XLPagerTabStrip
 
-class ListViewVC: UIViewController, IndicatorInfoProvider {
+class ListViewVC: UIViewController, IndicatorInfoProvider, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
         return IndicatorInfo(title: "LIST")
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
 }
