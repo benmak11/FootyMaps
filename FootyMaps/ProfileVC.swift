030bd9f0f72272e4cf3144c677780fa4c2084469
Ben Makusha <btmakusha@gmail.com>
2017-04-20 22:55:35 -0400

    completed sign out feature

--- FootyMaps/ProfileVC.swift
+++ FootyMaps/ProfileVC.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import Firebase
+import SwiftKeychainWrapper
 
 class ProfileVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func signOutButtonPushed(_ sender: Any) {
+        let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
+        print("BEN: removed id from keychain \(keychainResult)")
+        try! FIRAuth.auth()?.signOut()
+        performSegue(withIdentifier: "goToSignIn", sender: nil)
+    }
 }
