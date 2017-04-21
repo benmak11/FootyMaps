7bf0c7bf160d167e9d626afa77e5b38e1e67b554
Ben Makusha <btmakusha@gmail.com>
2017-04-20 20:31:58 -0400

    Added new content and tableViews to work with

---
+++ FootyMaps/FriendsVC.swift
@@ -0,0 +1,19 @@
+//
+//  FriendsVC.swift
+//  FootyMaps
+//
+//  Created by Ben Makusha on 4/20/17.
+//  Copyright © 2017 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+class FriendsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
