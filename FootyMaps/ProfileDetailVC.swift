e3e0bc2038487682f1bfa1302a54b58d41ab5b89
Ben Makusha <btmakusha@gmail.com>
2017-04-26 22:19:58 -0400

    Added profile detail VC

---
+++ FootyMaps/ProfileDetailVC.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileDetailVC.swift
+//  FootyMaps
+//
+//  Created by Ben Makusha on 4/26/17.
+//  Copyright © 2017 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+class ProfileDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
